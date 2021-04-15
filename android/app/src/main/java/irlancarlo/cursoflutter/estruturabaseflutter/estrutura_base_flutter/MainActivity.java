9384c052679874ee97a4d94e0348d239fe69c18b
IrlanGomesPessoal <irlan.carlo@gmail.com>
2021-04-15 10:32:54 -0400

    add estrutura base

---
+++ android/app/src/main/java/irlancarlo/cursoflutter/estruturabaseflutter/estrutura_base_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package irlancarlo.cursoflutter.estruturabaseflutter.estrutura_base_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
