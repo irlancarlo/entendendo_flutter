8c5c8ae3880c81a7f80ec4f1d9d77e3842f04d42
IrlanGomesPessoal <irlan.carlo@gmail.com>
2021-04-15 12:02:05 -0400

    aplicando formatações em textos

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
-
-
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    title: "Frases do dia",
-    // home: Container(color: Colors.white,),
-    // home: Column(
-    //   children: [
-    //     Text("texto 1"),
-    //     Text("texto 2"),
-    //     Text("texto 3"),
-    //     Text("texto 4"),
-    //   ],
-    // ),
-    home: Row(children: [
-      Text("T1,"),
-      Text("T2,"),
-      Text("T3,"),
-      Text("T4"),
-    ],),
-  ));
+      title: "Frases do dia",
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Text(
+              "Lorem Ipsum is simply dummy text of the printing and "
+              "typesetting industry.",
+              style: TextStyle(
+                  fontSize: 40,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                  // espaco entre as letras
+                  letterSpacing: 10,
+                  // espaco entre as palavras
+                  wordSpacing: 10,
+                  // define a posicao da linha
+                  decoration: TextDecoration.underline,
+                  // cor da linha do textp
+                  decorationColor: Colors.red,
+                  // define o tipo da linha
+                  decorationStyle: TextDecorationStyle.wavy,
+                  //
+                  color: Colors.black),
+            )
+          ],
+        ),
+      )));
 }
