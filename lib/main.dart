bcd4e2a3a9801a0e60bc45c038cef9973e1b79a2
IrlanGomesPessoal <irlan.carlo@gmail.com>
2021-04-15 10:41:18 -0400

    exemplo de container, column e row

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,20 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     title: "Frases do dia",
-    home: Container(color: Colors.white,),
+    // home: Container(color: Colors.white,),
+    // home: Column(
+    //   children: [
+    //     Text("texto 1"),
+    //     Text("texto 2"),
+    //     Text("texto 3"),
+    //     Text("texto 4"),
+    //   ],
+    // ),
+    home: Row(children: [
+      Text("T1,"),
+      Text("T2,"),
+      Text("T3,"),
+      Text("T4"),
+    ],),
   ));
 }
